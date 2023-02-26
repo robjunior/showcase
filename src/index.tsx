826c1b6fc4937e3f76bec58056d80cb4ab6fb7b9
Paulo Roberto <contato@robjunior.com>
2023-02-26 19:39:33 -0300

    Fix: Move mock api to another project

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import axios from 'axios';
 import App from './App';
+axios.defaults.baseURL = 'https://mock-api-zeta.vercel.app';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
