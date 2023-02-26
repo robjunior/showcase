59dee2f189137d3de1b754233df7940f5891ac05
Paulo Roberto <contato@robjunior.com>
2023-02-26 19:09:54 -0300

    Fix: Vercel Deploy of the Mock API

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import axios from 'axios';
 import App from './App';
+axios.defaults.baseURL = 'http://localhost:3001';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
